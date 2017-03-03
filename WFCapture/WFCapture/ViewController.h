2af0d9ca7a43a83e07cb4c9f4f7aff161a15cf10
babywolf <591138406@qq.com>
2017-03-03 23:15:19 +0800

    initialize project

---
+++ WFCapture/WFCapture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WFCapture
+//
+//  Created by babywolf on 17/3/3.
+//  Copyright © 2017年 babywolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
