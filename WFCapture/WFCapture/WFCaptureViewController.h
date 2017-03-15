dfc823dbb8217c48dc2a9585be37be861fa36990
babywolf <591138406@qq.com>
2017-03-15 21:43:33 +0800

    init WFCaptureViewController

---
+++ WFCapture/WFCapture/WFCaptureViewController.h
@@ -0,0 +1,13 @@
+//
+//  WFCaptureViewController.h
+//  WFCapture
+//
+//  Created by babywolf on 17/3/15.
+//  Copyright © 2017年 babywolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WFCaptureViewController : UIViewController
+
+@end
