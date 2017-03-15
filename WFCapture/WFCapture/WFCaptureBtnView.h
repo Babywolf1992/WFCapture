c1df98ceb07f11197b3bcf6d82e0cab3436ea6a0
babywolf <591138406@qq.com>
2017-03-15 21:52:52 +0800

    init WFCaptureBtnView

---
+++ WFCapture/WFCapture/WFCaptureBtnView.h
@@ -0,0 +1,13 @@
+//
+//  WFCaptureBtnView.h
+//  WFCapture
+//
+//  Created by babywolf on 17/3/15.
+//  Copyright © 2017年 babywolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WFCaptureBtnView : UIView
+
+@end
---
+++ WFCapture/WFCapture/WFCaptureBtnView.m
@@ -0,0 +1,21 @@
+//
+//  WFCaptureBtnView.m
+//  WFCapture
+//
+//  Created by babywolf on 17/3/15.
+//  Copyright © 2017年 babywolf. All rights reserved.
+//
+
+#import "WFCaptureBtnView.h"
+
+@implementation WFCaptureBtnView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
